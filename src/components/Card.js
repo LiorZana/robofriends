a87f9134200a594185f84826c8a894ce46be6fa7
LiorZana <liorzana@gmail.com>
2020-12-08 15:09:24 +0200

    finished the project after adding redux

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@
 const Card = ({name, email, id}) => {
     return(
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img id="profPic" alt='robotProfile' src={`https://api.adorable.io/avatars/150/${name + id}@adorable.io.png`} />
+            <img id="profPic" alt='robotProfile' src={`https://avatars.dicebear.com/4.5/api/human/${name + id}.svg`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
